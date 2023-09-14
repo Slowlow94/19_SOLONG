84e94e3f459cca454c5ea35940b1437374e4e902
SLOWIE <113249862+Slowlow94@users.noreply.github.com>
2023-09-14 13:58:14 +0200

    working

--- INCS/so_long.h
+++ INCS/so_long.h
@@ -6,18 +6,20 @@
 /*   By: salowie <salowie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:29:12 by salowie           #+#    #+#             */
-/*   Updated: 2023/09/12 18:40:57 by salowie          ###   ########.fr       */
+/*   Updated: 2023/09/14 13:53:52 by salowie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 #define SO_LONG_H
 
+#include "../../GET_NEXT_LINE/get_next_line.h"
+#include "../../PRINTF/ft_printf.h"
+
 # include <mlx.h>
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <stdio.h>
 
 typedef struct	s_img_struct {
 	void	*ptr_img;
@@ -26,6 +28,15 @@ typedef struct	s_img_struct {
 
 }				t_img_struct;
 
+typedef struct	s_list
+{
+	char			*content;
+	int				size;
+	struct s_node	*next;
+}				t_list;
+
 void	*ft_error(void);
+char	**convert_ber();
+t_list	*collect_strings(int fd);
 
 #endif
--- SRC/main.c
+++ SRC/main.c
@@ -6,7 +6,7 @@
 /*   By: salowie <salowie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 14:13:11 by salowie           #+#    #+#             */
-/*   Updated: 2023/09/13 14:23:05 by salowie          ###   ########.fr       */
+/*   Updated: 2023/09/14 13:55:26 by salowie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,46 @@ void	*ft_error(void)
 	return (NULL);
 }
 
+char	**fill_map(char *str, char **map)
+{
+}
+
+t_list	*collect_strings(int fd)
+{
+	t_list *strings_collected;
+	char *str;
+
+	str = get_next_line(fd);
+	while (str)
+	{
+		
+	}
+}
+
+char	**convert_ber()
+{
+	int		fd;
+	char	*lib;
+	t_list	*strings_collected;
+
+	lib = "map.ber";
+	fd = open(lib, O_RDONLY);
+	strings_collected = collect_strings(fd);
+	return (map);
+}
+
 int	main(void)
 {
 	void			*mlx;
 	void			*window;
 	t_img_struct	img;
-	
+	int				fd;
+	char			*lib;
+	char			**map;
+
+	lib = "map.ber";
+	fd = open(lib, O_RDONLY);
+	map = convert_ber();
 	mlx = mlx_init();
 	if (!mlx)
 		ft_error();
